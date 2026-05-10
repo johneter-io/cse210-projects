0b660336ebb22152fa33e8455538b36afc4590c2
johneter-io <johneter-io>
2026-05-10 00:23:05 -0400

    Exercise 2 (completed)

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // if, else if, elif statements
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+            Console.WriteLine("Your efforts have paid off!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! Better luck next time!");
+            Console.WriteLine("Work smarter and you'll succeed!");
+        }
     }
 }
