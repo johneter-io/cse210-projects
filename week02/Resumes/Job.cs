e8daf18feb36fe5b1cea3ece6b3a5f84c9a3d8f0
johneter-io <johneter-io>
2026-05-16 15:30:58 -0400

    Learning Activity: Abstraction

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,12 @@
+using System;
+public class Job
+{
+    public string _jobTitle;
+    public string _jobCompany;
+    public int _startYear;
+    public int _endYear;
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_jobCompany}) {_startYear}-{_endYear}");
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._jobCompany = "Facebbok";
+        job1._startYear = 2013;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Design";
+        job2._jobCompany = "X";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Avril Langlois";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
+
+
+
