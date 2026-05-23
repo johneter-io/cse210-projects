d203a7beb8da6e867be123ce6c18ca24bf032dff
johneter-io <johneter-io>
2026-05-23 18:44:02 -0400

    Initial commit: Scripture Memorizer Program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,16 @@
+// ---------------------------- W03 Project: Scripture Memorizer Program ----------------------------
+// ---------------------------- Student: John Peter Joseph ----------------------------
+// ---------------------------- Course: CSE 210: Programming with Classes ----------------------------
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Psalms", 16, 5);
+        string text = "The Lord is the portion of mine inheritance and of my cup: thou maintainest my lot.";
+
+        Scripture scripture = new Scripture(reference, text);
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+    }
+}
